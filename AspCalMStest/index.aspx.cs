88eb0d63fe8578bb73776d774d485207b6cce777
darryl <darryl.j@manipal.edu>
2021-10-08 15:39:48 +0530

    First commit for testing MSTEST

---
+++ AspCalMStest/index.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspCalMStest
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int val1, val2;
+
+            bool flag1 = Int32.TryParse(tbVal1.Text, out val1);
+            bool flag2 = Int32.TryParse(tbVal2.Text, out val2);
+
+            if ((flag1 && flag2) == false)
+            {
+                lblResult.Text = "Kindly enter some values";
+                lblResult.Visible = true;
+            }
+            else
+            {
+
+                if (ddoperator.SelectedValue.ToString() != "-1")
+                {
+                    int value = Convert.ToInt32(ddoperator.SelectedValue);
+
+                    switch (value)
+                    {
+                        case 0:
+                            int result = Calmstestdll.Class1.Add(val1, val2);
+                            ResultDisplay(result);
+                            break;
+                        case 1:
+                            int result1 = Calmstestdll.Class1.Sub(val1, val2);
+                            ResultDisplay(result1);
+                            break;
+                        case 2:
+                            int result2 = Calmstestdll.Class1.Mul(val1, val2);
+                            ResultDisplay(result2);
+                            break;
+                        case 3:
+                            int result3 = Calmstestdll.Class1.Div(val1, val2);
+                            ResultDisplay(result3);
+                            break;
+
+                    }
+                }
+
+                else
+                {
+                    lblResult.Text = "Kindly select an operation";
+                    lblResult.Visible = true;
+                }
+            }
+
+            
+        }
+        public void ResultDisplay(int result)
+        {
+            lblResult.Text = result.ToString();
+            lblResult.Visible = true;
+        }
+    }
+}
---
+++ CalMStest/Class1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalMStest
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void Test_Add()
+        {
+            //Arrange
+            int expected = 12;
+            int val1 = 8;
+            int val2 = 4;
+
+            //Act
+            int actual = Calmstestdll.Class1.Add(val1, val2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_sub()
+        {
+            //Arrange
+            int expected = 4;
+            int val1 = 8;
+            int val2 = 4;
+
+            //Act
+            int actual = Calmstestdll.Class1.Sub(val1, val2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Test_mul()
+        {
+            //Arrange
+            int expected = 32;
+            int val1 = 8;
+            int val2 = 4;
+
+            //Act
+            int actual = Calmstestdll.Class1.Mul(val1, val2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Test_div()
+        {
+            //Arrange
+            int expected = 2;
+            int val1 = 8;
+            int val2 = 4;
+
+            //Act
+            int actual = Calmstestdll.Class1.Div(val1, val2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
